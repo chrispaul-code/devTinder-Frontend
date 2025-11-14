ce8c7c06ef0c42a209b5da26c58a004c583d692a
Christeen Paul <chrispaul1311@gmail.com>
2025-11-14 10:36:53 +0530

    /api

--- src/utils/userSlice.js
+++ src/utils/userSlice.js
@@ -2,13 +2,13 @@ import { createSlice } from "@reduxjs/toolkit";
 
 const userSlice =createSlice({
     name:'user',
-    initialState:[],
+    initialState:null,
     reducers:{
         addUser:(state,action)=>{
             return action.payload;
         },
         removeUser:(state,action)=>{
-           return null
+            state.filter((user)=>user._id!=action.payload)
         }
     }
 });
